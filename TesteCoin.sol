a0c12d9763b27e150813f3d3366dd7894dcc6100
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2022-01-28 23:57:46 -0300

    Inicia a implementação do padrão ERC20

--- TesteCoin.sol
+++ TesteCoin.sol
@@ -62,17 +62,37 @@ contract Ownable {
     }
 }
 
-contract BasicToken is Ownable {
+contract ERC20 {
+    function totalSupply() public view returns (uint);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+contract BasicToken is Ownable, ERC20 {
     using SafeMath for uint;
 
     string public constant name = "Test Coin";
     string public constant symbol = "TST";
     uint8 public constant decimals = 18;
-    uint public totalSupply;
+    uint internal _totalSupply;
     mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
 
     event Mint(address indexed to, uint tokens);
-    event Transfer(address indexed from, address indexed to, uint tokens);
+    
+    function totalSupply() public view returns (uint) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address tokenOwner) public view returns (uint balance) {
+        return balances[tokenOwner];
+    }
 
     function mint(address to, uint tokens) onlyOwner public {
         balances[to] = balances[to].add(tokens);
@@ -81,7 +101,7 @@ contract BasicToken is Ownable {
         emit Mint(to, tokens);
     }
 
-    function transfer(address to, uint tokens) public {
+    function transfer(address to, uint tokens) public returns (bool success) {
         require(balances[msg.sender] >= tokens);
         require(to != address(0));
 
@@ -89,5 +109,31 @@ contract BasicToken is Ownable {
         balances[to] = balances[to].add(tokens);
 
         emit Transfer(msg.sender, to, tokens);
+
+        return true;
+    }
+
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+
+        emit Approval(msg.sender, spender, tokens);
+
+        return true;
+    }
+
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        require(balances[from] >= tokens);
+        require(to != address(0));
+
+        balances[from] = balances[from].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+        allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
+        emit Transfer(from, to, tokens);
+
+        return true;
     }
 }
